748c05ef3fc9886cf33f97ca0e9cd7132b037ddf
dev-willem <willemaccoount@gmail.com>
2024-11-15 13:00:59 -0300

    Find By Game ID

--- dslist/src/main/java/com/willemberg/dslist/services/GameService.java
+++ dslist/src/main/java/com/willemberg/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.willemberg.dslist.services;
 
+import com.willemberg.dslist.dto.GameDTO;
 import com.willemberg.dslist.dto.GameMinDTO;
 import com.willemberg.dslist.entities.Game;
 import com.willemberg.dslist.repository.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -17,5 +20,10 @@ public class GameService {
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    public GameDTO findById(@PathVariable Long listId) {
+        Game result = gameRepository.findById(listId).get();
+        return new GameDTO(result);
     }
 }
